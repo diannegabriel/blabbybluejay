2fbd96aaa54b33b66cd815aeb2a928b05f9d8d09
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 19:14:14 -0400

    Remove the delivered piece

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -14,7 +14,6 @@ const Message = ({ message }) => {
           <time className="text-xs opacity-50">12:45</time>
         </div>
         <div className="chat-bubble">{message.text}</div>
-        <div className="chat-footer opacity-50">Delivered</div>
       </div>
     </div>
   );
