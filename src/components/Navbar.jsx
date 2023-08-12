e4629e228a383218ec4e82dcd41a9530e171e366
Dianne <mdianne.gabriel@gmail.com>
2023-08-11 22:49:28 -0400

    Apply flexbox in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="navbar bg-primary text-primary-content">
-      <div className="container-wrap">
+      <div className="container-wrap flex justify-between">
         <a className="btn btn-ghost normal-case text-xl">Blabby Blue Jay</a>
+        <button>Logout</button>
       </div>
     </nav>
   );
