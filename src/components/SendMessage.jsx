c6509c3eeb71c7a8364198c58cde0cd7a24ab6c8
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 18:40:11 -0400

    Stylize form and input textbox

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const SendMessage = () => {
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
-      <form>
-        <input type="text" />
+      <form className="container-wrap">
+        <input className="input" type="text" />
         <button>Send</button>
       </form>
     </div>
