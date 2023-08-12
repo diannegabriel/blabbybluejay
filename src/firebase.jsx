a662d4d527b5d200584a1fd830061139294eaffb
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 19:43:41 -0400

    Paste config from firebase to new firebase.jsx file

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC88qm_K6M9PYwSXpVcM-dVOoaH8ZrY4TE",
+  authDomain: "blabbybluejay-1c9ea.firebaseapp.com",
+  projectId: "blabbybluejay-1c9ea",
+  storageBucket: "blabbybluejay-1c9ea.appspot.com",
+  messagingSenderId: "1098026640990",
+  appId: "1:1098026640990:web:6b4ba74f5b8b27c8def6cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
