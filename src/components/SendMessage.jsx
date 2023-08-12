218fa90afb4e43f245ff4fea215aba63a023ed53
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 18:47:17 -0400

    Create handleSendMessage function

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 const SendMessage = () => {
   const [value, setValue] = useState("");
-  console.log(value)
+
+  const handleSendMessage = () => {
+    console.log(value);
+  };
+
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
-      <form className="container-wrap flex">
-        <input value={value} onChange={e => setValue(e.target.value)}
-        className="input w-full focus:outline-none bg-gray-100 rounded-r-none" type="text" />
-        <button className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
+      <form onSubmit={handleSendMessage} className="container-wrap flex">
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="input w-full focus:outline-none bg-gray-100 rounded-r-none"
+          type="text"
+        />
+        <button
+          type="submit"
+          className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm"
+        >
+          Send
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default SendMessage
+export default SendMessage;
