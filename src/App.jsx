8f3136e21d7a893045a558212b652ace0bc9d731
Dianne <mdianne.gabriel@gmail.com>
2023-08-11 23:03:45 -0400

    Set up chatbox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ChatRoom from "./components/ChatRoom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Navbar />
       <Login />
+      <ChatRoom />
     </div>
   );
 }
