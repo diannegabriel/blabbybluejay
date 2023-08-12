851d084f70602314e6d37d2cd16ef0e31860fbd1
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 18:45:43 -0400

    Update input

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -3,10 +3,12 @@ import { useState } from 'react'
 
 const SendMessage = () => {
   const [value, setValue] = useState("");
+  console.log(value)
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
       <form className="container-wrap flex">
-        <input className="input w-full focus:outline-none bg-gray-100 rounded-r-none" type="text" />
+        <input value={value} onChange={e => setValue(e.target.value)}
+        className="input w-full focus:outline-none bg-gray-100 rounded-r-none" type="text" />
         <button className="w-auto bg-gray-500 text-white rounded-r-lg px-5 text-sm">Send</button>
       </form>
     </div>
