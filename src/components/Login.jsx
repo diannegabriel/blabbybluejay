4b0290c4521c1b684cc97caf94b35f210dc95fb4
Dianne <mdianne.gabriel@gmail.com>
2023-08-11 22:59:52 -0400

    Change font colours

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <div className="hero h-[90vh] bg-base-200">
+      <div className="hero h-[90vh] bg-slate-300">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hey there!</h1>
-            <p className="py-6">
+            <h1 className="text-5xl font-bold text-blue-800">Hey there!</h1>
+            <p className="py-6 text-zinc-700">
               Chat with Blue Jays fans in real-time. Trade talks? Game day? Post season? Come by and chill!
             </p>
             <button className="btn btn-primary">Start Blabbing!</button>
