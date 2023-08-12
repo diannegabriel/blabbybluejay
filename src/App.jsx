792a84118ea2a5120f4723f71a3940462932462b
Dianne <mdianne.gabriel@gmail.com>
2023-08-11 21:56:49 -0400

    Remove DaisyUI as a dependency, use it as CDN

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 function App() {
-
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
-  )
+    <div>
+    </div>
+  );
 }
 
-export default App
+export default App;
