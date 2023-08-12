22ccb579f6d35b5eb2dec4712219f581de83c4bd
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 19:50:40 -0400

    Set up new State current User

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
     <AuthContext.Provider>
       {children}
