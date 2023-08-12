7e98bbf6029b0417235237265c4afe1de339baab
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 18:35:28 -0400

    Add Send message component in Chatroom

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ChatBox from './ChatBox'
+import SendMessage from './SendMessage'
 
 const ChatRoom = () => {
   return (
     <div>
       <ChatBox />
+      <SendMessage />
     </div>
   )
 }
