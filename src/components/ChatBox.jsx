b9ffd1f15b6e30b4e2a971584054c25632fb466d
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 18:52:55 -0400

    Add hardcoded messages in Chatbox

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const ChatBox = () => {
+  const messages = [
+    {
+      id: 1,
+      message: "Blue jays forever!"
+    },
+    {
+      id: 2,
+      message: "Big fan from Toronto."
+    }
+  ]
   return (
-    <div>
+    <div className="pb-44 pt-20 container-wrap">
       
     </div>
   )
