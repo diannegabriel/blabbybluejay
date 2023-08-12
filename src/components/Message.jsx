7e4df135f685bb29557d51a40e0ad866906a0458
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 19:14:44 -0400

    Remove hardcoded img

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -6,7 +6,7 @@ const Message = ({ message }) => {
       <div className="chat chat-start">
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
-            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src="" />
           </div>
         </div>
         <div className="chat-header">
