d35e811d4f92e7c3f221231ae4855517f5779fa1
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 19:50:02 -0400

    Provide the return

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,5 +2,9 @@ import { createContext } from "react";
 
 const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
-
+  return (
+    <AuthContext.Provider>
+      {children}
+    </AuthContext.Provider>
+  )
 }
