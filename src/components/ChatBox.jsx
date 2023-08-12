28e5911dd5cd9ebb3bac019e799e1c84bbe06a32
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 19:12:04 -0400

    Update hardcoded data

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -6,10 +6,12 @@ const ChatBox = () => {
     {
       id: 1,
       text: "Blue jays forever!",
+      author: "Maple"
     },
     {
       id: 2,
       text: "Big fan from Toronto.",
+      author: "Leafs"
     },
   ];
   return (
