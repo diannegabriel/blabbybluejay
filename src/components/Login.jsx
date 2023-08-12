a5e162cd3ada423a583c109cf405c818dcd23e4c
Dianne <mdianne.gabriel@gmail.com>
2023-08-11 22:31:28 -0400

    Edit text in Hero content

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,13 +6,11 @@ const Login = () => {
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+            <h1 className="text-5xl font-bold">Hey there!</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Chat with Blue Jays fans in real-time. Trade talks? Game day? Post season? Come by and chill!
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">Start Blabbing!</button>
           </div>
         </div>
       </div>
