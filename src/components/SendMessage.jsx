f3b3d14666b50fe69f91ef025b8e6aa9afd6f2d5
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 18:39:13 -0400

    Initialize form

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const SendMessage = () => {
   return (
     <div className="bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg">
-      
+      <form>
+        <input type="text" />
+        <button>Send</button>
+      </form>
     </div>
   )
 }
