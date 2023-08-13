57c1aa5f1538ae5534e5c31f690c85df117f28bf
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 21:02:58 -0400

    Paste code from firebase

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import Message from "./Message";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { useEffect } from "react";
 
 const ChatBox = () => {
   const messages = [
     {
       id: 1,
       text: "Blue jays forever!",
-      author: "Maple"
+      author: "Maple",
     },
     {
       id: 2,
       text: "Big fan from Toronto.",
-      author: "Leafs"
+      author: "Leafs",
     },
   ];
+
+  useEffect(() => {
+    const q = query(collection(db, "cities"), where("state", "==", "CA"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const cities = [];
+      querySnapshot.forEach((doc) => {
+        cities.push(doc.data().name);
+      });
+      console.log("Current cities in CA: ", cities.join(", "));
+    });
+  }, []);
   return (
     <div className="pb-44 pt-20 container-wrap">
       {messages.map((message) => (
