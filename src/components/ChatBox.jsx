e05bc12a9073bbfbecec33769d385cc19976d2f4
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 21:02:07 -0400

    Import firestore in chatbox

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Message from "./Message";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
 
 const ChatBox = () => {
   const messages = [
