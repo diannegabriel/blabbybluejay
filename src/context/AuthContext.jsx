47cda96144741c311cd90e717cd1ed09b7d11482
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 20:02:47 -0400

    Paste the code from firebase

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -7,6 +7,8 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
+  // Sign in with Google
+  const signInGoogle = new GoogleAuthProvider();
 
   const value = {
     currentUser,
