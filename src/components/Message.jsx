4d61c10436cd3b2e39ac20a99209756b1672d761
Dianne <mdianne.gabriel@gmail.com>
2023-08-12 22:14:31 -0400

    Fix flex alignment for chat header

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Message = ({ message }) => {
+  const { currentUser } = UserAuth();
   return (
     <div>
-      <div className="chat chat-start">
+      <div
+        className={`chat ${
+          message.uid === currentUser.uid ? "chat-end" : "chat-start"
+        }`}
+      >
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img src={message.avatar} />
           </div>
         </div>
-        <div className="chat-header flex flex-col	items-baseline gap-x-1.5">
+        <div
+          className={`chat-header flex flex-col ${
+            message.uid === currentUser.uid ? "items-end" : ""
+          }`}
+        >
+          {/* // "chat-header flex flex-col	items-baseline gap-x-1.5" */}
           {message.name}
-          <time className="text-xs opacity-50">{new Date(message.createdAt.seconds*1000).toLocaleString()}</time>
+          <time className="text-xs opacity-50">
+            {new Date(message.createdAt.seconds * 1000).toLocaleString()}
+          </time>
         </div>
         <div className="chat-bubble">{message.text}</div>
       </div>
